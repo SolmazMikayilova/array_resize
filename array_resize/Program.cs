552b6ef842b1829cabea3ac8da3d2f9cd5154963
SolmazMikayilova <mikayilovasolmaz2@gmail.com>
2023-03-05 19:34:32 +0400

    resize task

---
+++ array_resize/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+bool flag = true;
+int increment = 0;
+Console.WriteLine("Please enter the size of the array");
+int sizeArray = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[0];
+do
+{
+    Console.WriteLine("Enter element ");
+    int element = Convert.ToInt32(Console.ReadLine());
+   Array.Resize(ref numbers, increment+1);
+    numbers[increment] = element;
+    increment++;
+    Console.WriteLine("Do you want to continue? ");
+    string answer = Console.ReadLine();
+    if (!(answer == "y" || answer == "Y"))
+    {
+        flag = false;
+        Console.WriteLine(".......exiting");
+    }
+
+} while (flag);
+Console.WriteLine("elements:");
+
+foreach (int elements in numbers)
+{
+    Console.WriteLine(elements);
+}
